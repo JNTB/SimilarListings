6f4b25866cba26ba4ae3f983f6b3eca656eb36d7
Merlin Chiang <merlinchiang@gmail.com>
2018-10-23 19:02:37 -0700

    set up server, webpack config, installed dependencies

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const db = require('../database');
+
+const app = express();
+const port = 1128;
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.listen(port, () => console.log(`server listening on port: ${port}`));
