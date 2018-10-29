2e7e4640d8cb0118884b20a98f9e1baaf24954c6
Merlin Chiang <merlinchiang@gmail.com>
2018-10-29 10:55:37 -0700

    small experiment with arrow absolute positioning

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -8,14 +8,13 @@ function NextArrow(props) {
   const { onClick } = props;
   return (
     <div
-      className="slick-arrow"
+      className="arrow-next"
       style={{ display: "block", background: "red" }}
-      onClick={onClick}
-    >
-    <svg viewBox="0 0 18 18" role="img" focusable="false" style={{height: "24px", width: "24px", display: "block", fill: "rgb(118, 118, 118)"}}>
-      <path d="m4.29 1.71a1 1 0 1 1 1.42-1.41l8 8a1 1 0 0 1 0 1.41l-8 8a1 1 0 1 1 -1.42-1.41l7.29-7.29z" fillRule="evenodd">
-      </path>
-    </svg>
+      onClick={onClick}>
+      <svg viewBox="0 0 18 18" role="img" focusable="false" style={{height: "24px", width: "24px", display: "block", fill: "rgb(118, 118, 118)"}}>
+        <path d="m4.29 1.71a1 1 0 1 1 1.42-1.41l8 8a1 1 0 0 1 0 1.41l-8 8a1 1 0 1 1 -1.42-1.41l7.29-7.29z" fillRule="evenodd">
+        </path>
+      </svg>
     </div>
   );
 }
@@ -24,14 +23,13 @@ function PrevArrow(props) {
   const { onClick } = props;
   return (
     <div
-      className="slick-arrow"
+      className="arrow-prev"
       style={{ display: "block", background: "green" }}
-      onClick={onClick}
-    >
-    <svg viewBox="0 0 18 18" role="img" focusable="false" style={{height: "24px", width: "24px", display: "block", fill: "rgb(118, 118, 118)"}}>
-      <path d="m13.7 16.29a1 1 0 1 1 -1.42 1.41l-8-8a1 1 0 0 1 0-1.41l8-8a1 1 0 1 1 1.42 1.41l-7.29 7.29z" fillRule="evenodd">
-      </path>
-    </svg>
+      onClick={onClick}>
+      <svg viewBox="0 0 18 18" role="img" focusable="false" style={{height: "24px", width: "24px", display: "block", fill: "rgb(118, 118, 118)"}}>
+        <path d="m13.7 16.29a1 1 0 1 1 -1.42 1.41l-8-8a1 1 0 0 1 0-1.41l8-8a1 1 0 1 1 1.42 1.41l-7.29 7.29z" fillRule="evenodd">
+        </path>
+      </svg>
     </div>
   );
 }
